0ab50360eb4a5bb6a77429fb2fc7a71c5df936ac
Dmitriy_Pyanzin <lysy_41@mail.ru>
2022-06-27 20:29:09 +0300

    Added_Task1_For_Topic1_Seminare1

---
+++ 1._Introduction_to_programming_languages/Seminars/Lesson_1._Introdutions_to_C#/Task1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите первое число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+if(numberB == numberA * numberA)
+{
+    Console.WriteLine($"Второе число {numberB} является квадратом первого числа {numberA}");
+}
+else
+{
+    Console.WriteLine($"Второе число {numberB} не является квадратом первого числа {numberA}");
+}
