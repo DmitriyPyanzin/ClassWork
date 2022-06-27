2e8e4bca9e7eb8da0096efa17cbb2a8f98bf6c4e
Dmitriy_Pyanzin <lysy_41@mail.ru>
2022-06-27 19:56:46 +0300

    Added_Task0_For_Topic1_Seminar1

---
+++ 1._Introduction_to_programming_languages/Seminars/Lesson_1._Introdutions_to_C#/Task0/Program.cs
@@ -0,0 +1,13 @@
+﻿//Напишите программу, которая на вход принимает число и выдает его квадрат (число умноженное на само себя).
+//цикл
+//ветвление
+
+//<тип_данных>  <имя_переменной>  =  <значения>;
+//<тип_данных>  <имя_переменной>;
+//int number = 5; //integer
+Console.Write("Введите число, которое нужно возвести в квадрат ");
+int number = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine(number * number);
+//Console.WriteLine(Math.Pow(number, 2));
+//Console.WriteLine("5 в квадрате равно " + Math.Pow(number, 2));
+Console.WriteLine($"{number} в квадрате равно {number * number}");
