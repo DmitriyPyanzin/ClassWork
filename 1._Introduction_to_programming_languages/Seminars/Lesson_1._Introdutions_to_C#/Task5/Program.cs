1eff3702433685de236b87684ffcbf8084ef3965
Dmitriy_Pyanzin <lysy_41@mail.ru>
2022-06-28 07:16:39 +0300

    Added_Task5_For_Topik1_Seminare1

---
+++ 1._Introduction_to_programming_languages/Seminars/Lesson_1._Introdutions_to_C#/Task5/Program.cs
@@ -0,0 +1,13 @@
+﻿int number = 6;
+//Console.WriteLine(number * number);
+//Console.WriteLine(Math.Pow(number, 2));
+number = 2;
+OurPower(number); // Вызов функции
+OurPower(7); // Вызов функции
+OurPower(number + number); // Вызов функции
+//OurPower(1.5); // Вызов функции
+
+void OurPower(int currentNumber)
+{
+    Console.WriteLine(currentNumber * currentNumber);
+}
