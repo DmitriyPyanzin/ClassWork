ae6ede4e6c64bddc011ec82e495a610882f276bd
Dmitriy_Pyanzin <lysy_41@mail.ru>
2022-06-28 07:52:30 +0300

    Added_Task7_For_Topik1_Seminare1

---
+++ 1._Introduction_to_programming_languages/Seminars/Lesson_1._Introdutions_to_C#/Task7/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine();
+Console.WriteLine("_______________________");
+Console.WriteLine("Task_7");
+int number = -4; //-4 -3 -2 -1 0 1 2 3 4
+number = Math.Abs(number);
+int count = -number;
+while(count <= number)
+{
+    Console.Write(count + " ");
+    count++;
+}
+Console.WriteLine();
