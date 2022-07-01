fbbfcbe653d7fdef9a1a573d18190c82ef7c109e
Dmitriy_Pyanzin <lysy_41@mail.ru>
2022-07-01 19:12:53 +0300

    Added_Task11.2_for_Topik1_Seminare2

---
+++ 1._Introduction_to_programming_languages/Seminars/Lesson_2._Arrays_and_Functions_in_Programming/Task11.2/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число: ");
+string number = Console.ReadLine();
+TripleNumber(number);
+
+void TripleNumber(string number)
+{
+    Console.WriteLine(number);
+    Console.Write(number[0]);
+    Console.Write(number[2]);
+    Console.WriteLine();
+}
