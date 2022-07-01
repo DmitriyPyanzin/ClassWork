03b80ebcd723c6c380866c2539206c5ee25c17f1
Dmitriy_Pyanzin <lysy_41@mail.ru>
2022-07-01 19:50:29 +0300

    Added_Task12_for_Topic1_Seminare2

---
+++ 1._Introduction_to_programming_languages/Seminars/Lesson_2._Arrays_and_Functions_in_Programming/Task12/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+if(num1 != 0)
+{
+    int div = num2 % num1; 
+    Zadacha12(div);
+}
+else Console.WriteLine("Ну и что же ты ввел?");
+
+void Zadacha12(int currentNumber)
+{
+    if(currentNumber == 0) Console.WriteLine("Yes");
+    else if(num1 == 0) Console.WriteLine("Ну только не 0");
+    else Console.WriteLine(currentNumber);
+}
