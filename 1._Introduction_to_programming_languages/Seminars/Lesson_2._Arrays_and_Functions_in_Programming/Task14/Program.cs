2c09ab5e36feeab95fd1e9c9c4d6459ab0cf4d32
Dmitriy_Pyanzin <lysy_41@mail.ru>
2022-07-01 20:03:37 +0300

    Added_Task14_for_Topik1_seminare1

--- 1._Introduction_to_programming_languages/Seminars/Lesson_2._Arrays_and_Functions_in_Programming/Task14/Program.cs
+++ 1._Introduction_to_programming_languages/Seminars/Lesson_2._Arrays_and_Functions_in_Programming/Task14/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if(num % 7 == 0 && num % 23 == 0) Console.WriteLine("Yes");
+else Console.WriteLine("No");
