6da717b06a212958ca570018f39a0bb8c66b10f8
Dmitriy_Pyanzin <lysy_41@mail.ru>
2022-07-01 18:58:00 +0300

    Added_NAsk11.1_for_Topic1_seminare2

---
+++ 1._Introduction_to_programming_languages/Seminars/Lesson_2._Arrays_and_Functions_in_Programming/Task11/Program.cs
@@ -0,0 +1,9 @@
+﻿Random rand = new Random();
+int number = rand.Next(100, 1000);
+FindNumber(number);
+
+void FindNumber(int currentNumber)
+{
+    Console.WriteLine($"Случайное число равно {currentNumber}");
+    Console.Write($"Новое число {currentNumber / 100 * 10 + currentNumber % 10}");    
+}
