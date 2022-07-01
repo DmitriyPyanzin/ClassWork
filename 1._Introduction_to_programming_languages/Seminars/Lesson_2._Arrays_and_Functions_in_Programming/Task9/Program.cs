5f213f0482a0618934c5ae0b11635e0d5962de47
Dmitriy_Pyanzin <lysy_41@mail.ru>
2022-07-01 18:32:02 +0300

    Added_Task9_for_Topic1_Seminare2

---
+++ 1._Introduction_to_programming_languages/Seminars/Lesson_2._Arrays_and_Functions_in_Programming/Task9/Program.cs
@@ -0,0 +1,20 @@
+﻿//9. Напишите программу, которая выводит случайное число из отрезка [10, 99]
+//и показывает наибольшую цифру числа.
+
+Random rand = new Random();
+int number = rand.Next(10, 100);
+Zadacha9(number);
+
+void Zadacha9(int currentNamber)
+{
+    Console.WriteLine(currentNamber);
+    int digitFirst = currentNamber / 10,
+        digitSecond = currentNamber % 10;
+
+    if(digitFirst > digitSecond) 
+       Console.WriteLine(digitFirst);
+    else if (digitFirst < digitSecond) 
+            Console.WriteLine(digitSecond);
+    else 
+        Console.WriteLine("Числа равны!");
+}
