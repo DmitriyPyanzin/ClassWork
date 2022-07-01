09a42283942f92d55c23a3f7860d70092f24dac4
Dmitriy_Pyanzin <lysy_41@mail.ru>
2022-07-01 16:40:32 +0300

    Added_Example010_MethodArray

--- 1._Introduction_to_programming_languages/Lectures/Lesson_2._Introduction_to_functuons_and_arrays/Example010_MethodArray/Program.cs
+++ 1._Introduction_to_programming_languages/Lectures/Lesson_2._Introduction_to_functuons_and_arrays/Example010_MethodArray/Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿int[] array = {12, 91, 65, 52, 18, 59, 61, 18, 80};
+int n = array.Length;
+int find = 18;
+int index = 0;
+
+while(index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
