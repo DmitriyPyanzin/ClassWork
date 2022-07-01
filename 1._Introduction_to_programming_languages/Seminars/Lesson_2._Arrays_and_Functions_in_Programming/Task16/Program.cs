a22addca3ad0837cf58cfa0a56cc721be2df070f
Dmitriy_Pyanzin <lysy_41@mail.ru>
2022-07-01 20:24:34 +0300

    Added_Task16_for_Topic1_Seminare2

---
+++ 1._Introduction_to_programming_languages/Seminars/Lesson_2._Arrays_and_Functions_in_Programming/Task16/Program.cs
@@ -0,0 +1,10 @@
+﻿void FindNumber(int number, int currentNumber)
+{
+    if(number * number == currentNumber) Console.WriteLine("Yes");
+    else Console.WriteLine("NO");
+}
+
+Console.Write("Введите два числа: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+int num2 = Convert.ToInt32(Console.ReadLine());
+FindNumber(num1, num2);
