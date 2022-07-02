1f49ca1f8b6d0519b922f6604cd6b1bc8a4e8431
Dmitriy_Pyanzin <lysy_41@mail.ru>
2022-07-02 23:14:27 +0300

    Added_Task21_for_ClassWork_Topic1_Seminare3

---
+++ 1._Introduction_to_programming_languages/Seminars/Lesson_3._Arrays_and_Functions_in_Programming/Task21/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите координаты первой точки: ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+int y1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координаты второй точки: ");
+int x2 = Convert.ToInt32(Console.ReadLine());
+int y2 = Convert.ToInt32(Console.ReadLine());
+FindLen(x1, y1, x2, y2);
+
+void FindLen(int xa, int ya, int xb, int yb)
+{
+    double A = Math.Pow(xa - xb, 2);
+    double B = Math.Pow(ya - yb, 2);
+    Console.WriteLine($"Длинна отрезка равна {Math.Sqrt(A + B)}");
+}
