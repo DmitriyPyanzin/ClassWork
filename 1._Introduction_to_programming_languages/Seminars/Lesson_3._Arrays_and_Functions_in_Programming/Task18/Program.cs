3590a86a99f35dbba850988d86e237cec39bdb1a
Dmitriy_Pyanzin <lysy_41@mail.ru>
2022-07-02 22:49:49 +0300

    Added_Task18_ClassWork_for_Topic1_Seminare3

---
+++ 1._Introduction_to_programming_languages/Seminars/Lesson_3._Arrays_and_Functions_in_Programming/Task18/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите число от 1 до 4: ");
+int number = Convert.ToInt32(Console.ReadLine());
+FindCoord(number);
+
+void FindCoord(int num)
+{
+    if(num == 1) Console.WriteLine("X > 0 and Y > 0");
+    else if(num == 2) Console.WriteLine("X < 0 and Y > 0");
+    else if(num == 3) Console.WriteLine("X < 0 and Y < 0");
+    else if(num == 4) Console.WriteLine("X > 0 and Y < 0");
+    else Console.WriteLine("Ты что-то не то ввел (-а)!");
+}
