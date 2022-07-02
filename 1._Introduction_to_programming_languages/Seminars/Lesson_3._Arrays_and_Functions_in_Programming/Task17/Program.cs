af86a2ae3d90eb3a9a733cbaa10b8554e680f11d
Dmitriy_Pyanzin <lysy_41@mail.ru>
2022-07-02 22:27:05 +0300

    Added_Task17_ClassWork_for_Topic1_Seminare3

---
+++ 1._Introduction_to_programming_languages/Seminars/Lesson_3._Arrays_and_Functions_in_Programming/Task17/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите два числа: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+int num2 = Convert.ToInt32(Console.ReadLine());
+FindQuarter(num1, num2);
+
+void FindQuarter(int x, int y)
+{
+    if(x > 0 && y > 0) Console.WriteLine("I-я четверть;");
+    else if(x > 0 && y < 0) Console.WriteLine("IV-я четверть;");
+    else if(x < 0 && y > 0) Console.WriteLine("II-я четверть;");
+    else if(x < 0 && y < 0) Console.WriteLine("III-я четверть;");
+    else Console.WriteLine("Точка находится на координатной прямой!");
+}
