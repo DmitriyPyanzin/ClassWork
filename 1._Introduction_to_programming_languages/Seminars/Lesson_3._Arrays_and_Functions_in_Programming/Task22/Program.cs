126203062f560d2be4eeb1f5dc0cf78bef3c7ab3
Dmitriy_Pyanzin <lysy_41@mail.ru>
2022-07-02 23:53:17 +0300

    Added_Task22_for_SlassWork_Topic1_Seminare3

---
+++ 1._Introduction_to_programming_languages/Seminars/Lesson_3._Arrays_and_Functions_in_Programming/Task22/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите число: ");
+int count = Convert.ToInt32(Console.ReadLine());
+SquaresNambers(count);
+
+void SquaresNambers(int num)
+{
+    for(int i = 1; i <= num; i++)
+    {
+        Console.Write(i * i + " ");
+    }
+    
+}
