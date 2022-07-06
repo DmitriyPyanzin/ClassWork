27abb6de485e04d44438ca6aefc38d846d57b032
Dmitriy_Pyanzin <lysy_41@mail.ru>
2022-07-06 17:09:05 +0300

    Added_Task28_Topic1_Seminare4

---
+++ 1._Introduction_to_programming_languages/Seminars/Lesson_4._Functions_continued/Task28/Program.cs
@@ -0,0 +1,21 @@
+﻿void Task28()
+{
+	//Задача 28: Напишите программу, которая принимает на вход число N 
+	//и выдаёт произведение чисел от 1 до N.
+	
+	Console.Write("Введите число: ");
+	int num = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine($"Факториал числа {num} равен {FindFact(num)}");	
+}
+
+int FindFact(int number)
+{
+	int fact = 1;
+	for(int i = 1; i <= number; i++)
+	{
+		fact *= i;
+	}
+	return fact;
+}
+
+Task28();
