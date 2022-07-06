592a936c13ad27dcde68b55565b837945e88ee29
Dmitriy_Pyanzin <lysy_41@mail.ru>
2022-07-06 17:00:38 +0300

    Added_Task25_for_ClassWork_Topic1_Seminare4

---
+++ 1._Introduction_to_programming_languages/Seminars/Lesson_4._Functions_continued/Task25/Program.cs
@@ -0,0 +1,21 @@
+﻿void Zadacha24()
+{
+    //Задача 24: Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+
+    Console.Clear();
+	Console.Write("Введите число: ");
+	int number = Convert.ToInt32(Console.ReadLine());
+	Console.WriteLine($"Сумма чисел от 1 до {number} равна {FindSum(number)}");
+}
+
+int FindSum(int number)
+{
+	int sum = 0;
+	for(int i = 1; i <= number; i++)
+	{
+		sum += i;
+	}
+	return sum;
+}
+
+Zadacha24();
