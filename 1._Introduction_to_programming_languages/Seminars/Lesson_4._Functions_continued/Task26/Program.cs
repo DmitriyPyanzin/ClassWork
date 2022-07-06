331316e0080ce2506221dccc7c7edd81411c2df9
Dmitriy_Pyanzin <lysy_41@mail.ru>
2022-07-06 17:05:38 +0300

    Added_Task26_for_ClassWork_Topic1_Seminare4

---
+++ 1._Introduction_to_programming_languages/Seminars/Lesson_4._Functions_continued/Task26/Program.cs
@@ -0,0 +1,22 @@
+﻿void Task26()
+{
+    //Задача 26: Напишите программу, которая принимает на вход число 
+    //и выдаёт количество цифр в числе.
+	
+	Console.Clear();
+	Console.Write("Введите число: ");
+	int number = Convert.ToInt32(Console.ReadLine());
+	Console.WriteLine($"Веденоое число {number} состоит из {NumbersofDigits(number)} цифр");
+}
+
+int NumbersofDigits(int number)
+{
+	int i;
+	for(i = 0; number > 0; i++)
+	{
+		number /= 10;
+	}
+	return i;
+}
+
+Task26();
